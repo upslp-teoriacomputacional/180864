07afec5cb168e36f9855b38c343bf2fc3a96441e
Juan Carlos Gonzalez Ibarra <jcarlos0284@gmail.com>
2021-02-15 16:32:01 -0600

    practica cadenas

---
+++ Strings/strings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 16:24:25 2021
+This program validate chars to alphabet {a-z, A-Z} and digits {0-9} 
+@author: jc-gi
+"""
+import re
+def special_match(strg, search=re.compile(r'[^a-z0-9.]').search):
+    return not bool(search(strg))
+
+#execute this command in terminal
+
+special_match("")#the result its true
+
+special_match("az09.") #the result its true
+
+special_match("az09.\n")#the result is false
+
+# The above test case is to catch out any attempt to use re.match()
+# with a `$` instead of `\Z` -- see point (6) below.
+special_match("az09.#")
+special_match("az09.X")
+
